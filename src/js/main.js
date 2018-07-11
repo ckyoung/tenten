cc0930141756b0aa4cde158b17575ffa4556d884
unknown <clarkkentyoung@gmail.com>
2018-07-11 21:28:02 +0800

    Add Vuejs and shift key function

---
+++ src/js/main.js
@@ -0,0 +1,24 @@
+new WOW().init();
+
+window.addEventListener('keydown', function(e) {
+  if(e.shiftKey) {
+    let shiftAlert = document.querySelector('#shift-alert');
+
+    shiftAlert.classList.add('alert');
+  }
+});
+
+window.addEventListener('keyup', function(e) {
+  if(e.keyCode == 16){
+    let shiftAlert = document.querySelector('#shift-alert');
+
+    shiftAlert.classList.remove('alert');
+  }
+});
+
+const app = new Vue({
+  el: '#root',
+  data: {
+    hamburgerToggle: false
+  }
+})
